5f71cc6a5c465cae27f8be192b36de2a4e248892
kindacommander <artem.sandyuk@gmail.com>
2020-11-21 22:22:39 +0200

    Symbols frequency sorting.

--- internal/counter/counter.go
+++ internal/counter/counter.go
@@ -1,13 +1,33 @@
 package counter
 
-func FreqCount(str string) map[string]int {
-	freqs := make(map[string]int)
+import "sort"
+
+type Char struct {
+	symbol string
+	freq   int
+}
+
+func FreqCount(str string) []Char {
+	var freqs []Char
 	for _, el := range str {
-		if _, exists := freqs[string(el)]; exists == false {
-			freqs[string(el)] = 1
+		if i, ok := exists(freqs, el); ok {
+			freqs[i].freq++
 		} else {
-			freqs[string(el)]++
+			freqs = append(freqs, Char{string(el), 1})
 		}
 	}
+	sort.SliceStable(freqs, func(i, j int) bool {
+		return freqs[i].freq > freqs[j].freq
+	})
+
 	return freqs
+}
+
+func exists(freqs []Char, symbol rune) (int, bool) {
+	for i, el := range freqs {
+		if el.symbol == string(symbol) {
+			return i, true
+		}
+	}
+	return -1, false
 }
